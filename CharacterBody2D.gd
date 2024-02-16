f7699eceabcb7b3200c4f67dc8b45eaddddf2d0a
Zirolet <zirolet@gmail.com>
2024-02-16 07:56:29 -0600

    fix: finished main logic

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var on_ladder = false
 var near_ladder = false
+var walkingAnims = [ResourceLoader.load("res://Britney/Idle.png"), ResourceLoader.load("res://Britney/Walk1.png"), ResourceLoader.load("res://Britney/Idle.png"), ResourceLoader.load("res://Britney/Walk2.png")]
+var walkingIndex = 0
 
 func _process(delta):
 	
@@ -10,15 +12,35 @@ func _process(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("right"):
 		velocity.x = 175
+		$TextureRect.scale.x = 1
+		walkingIndex += 0.08
+		if walkingIndex >= 4:
+			walkingIndex = 0
 	if Input.is_action_pressed("left"):
 		velocity.x = -175
+		$TextureRect.scale.x = -1
+		walkingIndex += 0.08
+		if walkingIndex >= 4: 
+			walkingIndex = 0
 	
 	if is_on_floor():
 		velocity.y = 0
+		if velocity.x == 0:
+			walkingIndex = 0
+		$TextureRect.texture = walkingAnims[floor(walkingIndex)]
 	if is_on_ceiling():
 		velocity.y = 1
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -300
+		$TextureRect.texture = ResourceLoader.load("res://Britney/Jump1.png")
+	if velocity.y > -300 and velocity.y < -280:
+		$TextureRect.texture = ResourceLoader.load("res://Britney/Jump2.png")
+	elif velocity.y > -280 and velocity.y < -100:
+		$TextureRect.texture = ResourceLoader.load("res://Britney/Jump3.png")
+	elif velocity.y > -100 and velocity.y < 10 and not velocity.y == 0:
+		$TextureRect.texture = ResourceLoader.load("res://Britney/Jump4.png")
+	if velocity.y > 10:
+		$TextureRect.texture = ResourceLoader.load("res://Britney/Jump5.png")
 		
 	if not on_ladder and Input.is_action_just_pressed("climb") and near_ladder:
 		on_ladder = true
